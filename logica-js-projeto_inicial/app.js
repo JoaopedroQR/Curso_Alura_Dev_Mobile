5e0ace19cfd1d9dc69de97072fd220adf737c4a1
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-11 14:06:31 -0300

    Boas práticas de programação

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,32 +1,33 @@
-// alert('Boas vindas ao jogo do número secreto');
-// let secretNumber = 2;
-// console.log('Valor do numero secreto: ' + secretNumber);
-// //Para criarmos variáveis e armazenar valor dentro utilizamos let
-// let chosenNumber;
-// let tentativas = 1;
+alert('Boas vindas ao jogo do número secreto');
+let secretNumber = parseInt(Math.random() * 100 + 1);
+console.log('Valor do numero secreto: ' + secretNumber);
+//Para criarmos variáveis e armazenar valor dentro utilizamos let
+let chosenNumber;
+let tentativas = 1;
 
-// //enquanto        diferente
-// while(chosenNumber != secretNumber){
-//     chosenNumber = prompt('Escolha um numero entre 1 e 10'); // Pedir dado externo para o usuário
-//     console.log('Valor do numero escolhido: ' + chosenNumber);
-//     if (secretNumber == chosenNumber){ // condição
-//         console.log('Resultado da comparação:', chosenNumber == secretNumber);
-//         // console.log('Isso aí! Você descobriu o número secreto(5)'); ferramenta para desenvolvedor nao interage com o usuario
-//         // alert('Isso aí! Você descobriu o número secreto'); 
-//         // alert(`Isso aí! Você descobriu o número secreto ${secretNumber}`); // template strings
-//         alert('Isso aí! Você descobriu o número secreto ' + secretNumber + ' com ' + tentativas + ' tentativas'); // Alternativa ao template strings
-//     } else{
-//         // tentativas = tentativas + 1; 
-//         tentativas++;
-//         if(secretNumber > chosenNumber){
-//             alert(`O número secreto é maior que ${chosenNumber}`);
-//         }else{
-//             alert(`O número secreto é menor que ${chosenNumber}`);
-//         }
-//     }
-// }
+//enquanto        diferente
+while(chosenNumber != secretNumber){
+    chosenNumber = prompt('Escolha um numero entre 1 e 100'); // Pedir dado externo para o usuário
+    console.log('Valor do numero escolhido: ' + chosenNumber);
+    if (secretNumber == chosenNumber){ // condição
+        break;
+    } else{
+        // tentativas = tentativas + 1; 
+        tentativas++;
+        if(secretNumber > chosenNumber){
+            alert(`O número secreto é maior que ${chosenNumber}`);
+        }else{
+            alert(`O número secreto é menor que ${chosenNumber}`);
+        }
+    }
+}
 
-
+// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+// console.log('Resultado da comparação:', chosenNumber == secretNumber);
+// // console.log('Isso aí! Você descobriu o número secreto(5)'); ferramenta para desenvolvedor nao interage com o usuario
+// // alert('Isso aí! Você descobriu o número secreto'); 
+// // alert(`Isso aí! Você descobriu o número secreto ${secretNumber}`); // template strings
+// alert('Isso aí! Você descobriu o número secreto ' + secretNumber + ' com ' + tentativas + ' ' + palavraTentativa); // Alternativa ao template strings
 
 
 // // DESAFIOS
@@ -138,3 +139,74 @@
 // O operador OR, representado pelos símbolos "||", é usado para verificar se pelo menos uma
 // das condições é verdadeira.
 // o operador NOT, representado pelos simbolos "!", é utilizado para inverter a veracidade da variável
+
+
+// // DESAFIOS 4
+// // 1. Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+// console.log('Boas-vindas, Querido(a)');
+// // 2. Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o
+// // console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+// let nome = 'João Pedro';
+// console.log( "Olá, " + nome + "!");
+// // 3. Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert
+// // para exibir a mensagem "Olá, [seu nome]!" .
+// let nome_alert = 'João Pedro';
+// alert( "Olá, " + nome_alert + "!");
+// // 4. Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?.
+// // Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+// let linguagem = prompt('Qual a linguagem de programação você mais gosta?');
+// console.log(linguagem);
+// // 5. Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos
+// // de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira
+// // variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é
+// // igual a [resultado]." no console.
+// let valor1 = 20;
+// let valor2 = 10;
+// let resultado = valor1 + valor2;
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+// // 6. Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha.
+// // Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada
+// // "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a
+// // [resultado]." no console.
+// let valor1_SUB = 40;
+// let valor2_SUB = 20;
+// let resultado_SUB = valor1_SUB - valor2_SUB;
+// console.log(`A subtração de ${valor1_SUB} e ${valor2_SUB} é igual a ${resultado_SUB}`);
+// // 7. Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a
+// // pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+// let idade = prompt('Escreva sua idade');
+// if (idade>=18){
+//     console.log('Você é maior de idade');
+// }else{
+//     console.log('Você não é maior de idade');
+// }
+// // 8. Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para
+// // imprimir a respectiva mensagem.
+// let numero = prompt('Escreva um numero inteiro');
+// if(numero>0){
+//     console.log('Numero positivo');
+// }else if(numero<0){
+//     console.log('Numero negativo');
+// }else{
+//     console.log('Numero nulo');
+// }
+// // 9. Use um loop while para imprimir os números de 1 a 10 no console.
+// let k = 0;
+// while(k<10){
+//     console.log(k+1);
+//     k++;
+// }
+// // 10. Crie uma variável "nota" e atribua um valor numérico a ela.
+// // Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+// let nota = 8;
+// if(nota>=7){
+//     console.log('Aprovado');
+// }else{
+//     console.log('Reprovado');
+// }
+// // 11. Use o Math.random para gerar  qualquer número aleatório e exiba esse número no console.
+// console.log(Math.random());
+// // 12. Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+// console.log(parseInt(Math.random() *10 + 1));
+// // 13. Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+// console.log(parseInt(Math.random() *100 + 1));
