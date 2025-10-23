21fefe8dafb7447ec496c503932c2750d79fa36f
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-23 10:55:22 -0300

    Funções

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,10 +1,25 @@
 //H1 titulo da página em si
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
+// p = Parágrafo
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 function verificarChute(){
-    console.log('O botão foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -21,3 +21,44 @@ function cliclarBotaoSoma(){
     numero2_inteiro = parseInt(numero2)
     alert(`O resultado da soma entre ${numero1} e ${numero2} é igual a ${numero1_inteiro + numero2_inteiro}`);
 }
+
+// Desafio 2:
+// 1. Criar uma função que exibe "Olá, mundo!" no console.
+function helloWorld(){
+    console.log('Olá, mundo!');
+}
+helloWorld();
+// 2. Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function saudacao(nome){
+    console.log('Olá ' +  nome + '!');
+}
+saudacao('João Pedro');
+// 3. Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function dobro(numero){
+    return numero*2;
+}
+numero_dobrado = dobro(4);
+console.log(numero_dobrado);
+// 4. Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function mediaArimetica(numero1, numero2, numero3){
+    return (numero1 + numero2 + numero3) / 3;
+}
+media_aritimetica = mediaArimetica(3,6,9);
+console.log(media_aritimetica);
+// 5. Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maior_numero(numero1, numero2){
+    if (numero1 > numero2){
+        return numero1;
+    }else{
+        return numero2;
+    }
+}
+maior = maior_numero(7,4);
+console.log(maior);
+// 6. Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação
+// desse número por ele mesmo
+function quadrado(numero1){
+    return numero1*numero1;
+}
+numeroElevado2 = quadrado(4);
+console.log(numeroElevado2);
