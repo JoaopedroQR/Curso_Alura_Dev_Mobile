c75f726d69db6739a90e867abe172148bd73c7ae
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-27 14:36:51 -0300

    revert "Testando conflito"
    This reverts commit f7d233938d574b72ca2b0d06aa6287424b7d8266.

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -6,7 +6,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 tentativas = 1;
 function verificarChute(){
     let chute = document.querySelector('input').value;
