b3132992a590145d33d3942d8ae08ca8cc263290
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-27 07:48:41 -0300

    ResponsiveVoice

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -52,6 +52,21 @@ function gerarNumeroAleatorio(){
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
+    exibirTextoNaTela(tag, texto);
+}
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 function exibirMensagemInicial(){
