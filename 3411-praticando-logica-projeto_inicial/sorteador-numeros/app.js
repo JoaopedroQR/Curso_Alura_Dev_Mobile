1d60cc7da922b298097a7ae31bbb46cea95be844
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-30 12:29:34 -0300

    Projeto sorteador de numeros

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,21 +1,31 @@
 function sortear(){
-    let quantidadeDeNumeros = document.getElementById('quantidade').value;
-    let valorMinimo = document.getElementById('de').value;
-    let valorMaximo = document.getElementById('ate').value;
+
+    let quantidadeDeNumeros = parseInt(document.getElementById('quantidade').value);
+    let valorMinimo = parseInt(document.getElementById('de').value);
+    let valorMaximo = parseInt(document.getElementById('ate').value);
     
     if (valorMinimo >= valorMaximo){
         alert("O numero final precisa ser maior que o inicial!!!");
         return;
     }
 
+    if(quantidadeDeNumeros > valorMaximo){
+        alert("Quantidade insuficiente de numeros para gerar uma lista (Aumente o campo 'até o número')");
+        return;
+    }
 
     let listaNumerosSorteados = [];
     geradorNumerosAleatorios(quantidadeDeNumeros,valorMinimo, valorMaximo, listaNumerosSorteados);
+    console.log(listaNumerosSorteados);
+
+    let tagFrase = document.getElementById('resultado'); // não possui .value pois queremos o elemento HTML
+    numeroSorteadoFrase = quantidadeDeNumeros > 1 ? "Numeros Sorteados" : "Numero Sorteado";
+    texto = `${numeroSorteadoFrase}: ${listaNumerosSorteados}`;
+    tagFrase.innerHTML = `<label class="texto__paragrafo">${texto}</label>`;
 
     document.getElementById('btn-sortear').setAttribute('disabled', true);
     document.getElementById('btn-reiniciar').removeAttribute('disabled');
 
-    console.log(listaNumerosSorteados);
 }
 
 function reiniciar(){
@@ -25,15 +35,16 @@ function reiniciar(){
 
 function geradorNumerosAleatorios(quantidadeDeNumeros,valorMinimo,valorMaximo, listaNumerosSorteados){
     let i = 0;
+    let verificaRepetidos = []
     while(i<quantidadeDeNumeros){
-        let numeroAleatorio =  parseInt((Math.random() * (valorMaximo-valorMinimo)) + valorMinimo);
-        listaNumerosSorteados.push(numeroAleatorio);
-        i++;
+        let numeroAleatorio =  parseInt((Math.random() * (valorMaximo-valorMinimo+1)) + valorMinimo);
+        if(verificaRepetidos.includes(numeroAleatorio)){
+            continue;
+        }else{
+            listaNumerosSorteados.push(numeroAleatorio);
+            verificaRepetidos.push(numeroAleatorio);
+            console.log(listaNumerosSorteados);
+            i++;
+        }
     }
-}
-
-function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-    exibirTextoNaTela(tag, texto);
 }
