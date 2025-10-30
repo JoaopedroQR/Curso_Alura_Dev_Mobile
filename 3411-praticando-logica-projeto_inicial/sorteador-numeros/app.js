aefbea1221dc3bdd2a7cde0d324d15155fa7aa8e
João Pedro Queiroz Rodrigues <Joaoprodrigues1108@gmail.com>
2025-10-30 20:14:38 -0300

    Projeto sorteador de numeros

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -4,6 +4,11 @@ function sortear(){
     let valorMinimo = parseInt(document.getElementById('de').value);
     let valorMaximo = parseInt(document.getElementById('ate').value);
     
+    if(Number.isNaN(quantidadeDeNumeros) || Number.isNaN(valorMaximo) || Number.isNaN(valorMinimo)){
+        alert("Insira os campos!!");
+        return;
+    }
+
     if (valorMinimo >= valorMaximo){
         alert("O numero final precisa ser maior que o inicial!!!");
         return;
@@ -16,21 +21,54 @@ function sortear(){
 
     let listaNumerosSorteados = [];
     geradorNumerosAleatorios(quantidadeDeNumeros,valorMinimo, valorMaximo, listaNumerosSorteados);
-    console.log(listaNumerosSorteados);
 
-    let tagFrase = document.getElementById('resultado'); // não possui .value pois queremos o elemento HTML
+    let tagFrase = document.getElementById('resultado'); // não possui .value pois queremos o elemento HTML;
     numeroSorteadoFrase = quantidadeDeNumeros > 1 ? "Numeros Sorteados" : "Numero Sorteado";
     texto = `${numeroSorteadoFrase}: ${listaNumerosSorteados}`;
     tagFrase.innerHTML = `<label class="texto__paragrafo">${texto}</label>`;
 
-    document.getElementById('btn-sortear').setAttribute('disabled', true);
-    document.getElementById('btn-reiniciar').removeAttribute('disabled');
+    alterarStatusBotaoSortear();
+    alterarStatusBotaoReiniciar();
+    // class referenciam a um trecho do código CSS;
+}
 
+function alterarStatusBotaoSortear(){
+    // pegar ID;
+    let botao = document.getElementById('btn-sortear');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function alterarStatusBotaoReiniciar(){
+    // pegar ID;
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
 }
 
 function reiniciar(){
-    document.getElementById('btn-sortear').removeAttribute('disabled');
-    listaDeNumerosSorteados = [];
+    let tagFrase = document.getElementById('resultado');
+    tagFrase.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    limparCampo('quantidade');
+    limparCampo('de');
+    limparCampo('ate');
+    alterarStatusBotaoSortear();
+    alterarStatusBotaoReiniciar();
+}
+
+function limparCampo(tag){
+    campo = document.getElementById(tag);
+    campo.value = '';
 }
 
 function geradorNumerosAleatorios(quantidadeDeNumeros,valorMinimo,valorMaximo, listaNumerosSorteados){
